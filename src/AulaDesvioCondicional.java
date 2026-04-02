3df74f2a22aa7afd0ec31038ef827bd2b6c9f081
VILSON MORO <vilson.moro@prof.sc.senac.br>
2026-04-02 14:51:23 -0300

    Criado solução do exercicio numero par

--- src/AulaDesvioCondicional.java
+++ src/AulaDesvioCondicional.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 
 public class AulaDesvioCondicional {
 
 	public static void main(String[] args) {
-		System.out.println("Ola");
+		verificarNumeroPar();
+	}
+	
+	static void verificarNumeroPar() {
+	   Scanner sc = new Scanner(System.in);	
+	   System.out.println("Informe numero: ");
+	   int numero = sc.nextInt();
+	   
+	   if(numero % 2 == 0) {
+		   System.out.println("Numero par");
+	   }
+	  
 	}
 
 }
