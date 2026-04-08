6d811de3ab1d3204700990bb97a87110ad03d51c
VILSON MORO <vilson.moro@prof.sc.senac.br>
2026-04-08 16:31:34 -0300

    adicionado conceitos de parametros

--- src/Vetores.java
+++ src/Vetores.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Vetores {
 
 	public static void main(String[] args) {
@@ -30,8 +32,31 @@ public class Vetores {
 		for(String c : cars ) {
 			System.out.println(c);	
 		}*/
-		desafioDobro();
-
+		//desafioDobro();
+		/*int[] numeros = {1,2,3,4,5};
+		listarArray(numeros);
+		
+		System.out.println("");
+		
+		int[] notas = {7,8,9,10,3};
+		listarArray(notas);*/
+	/*	int retorno = somar(10, 20);
+		System.out.println(retorno);
+		
+		retorno = somar(1000, 300);
+		System.out.println(retorno);*/
+		
+		double precoPizza = 150.00;
+		int numeroPessoas = 0;
+		System.out.println(dividir(precoPizza, numeroPessoas));
+	}
+	
+	static double dividir(double numero, int divisor) {
+		if(divisor == 0) {
+			JOptionPane.showMessageDialog(null, "Não pode dividir por zero");
+			return 0.0;
+		}
+		return numero / divisor;
 	}
 	
 	static void desafioDobro() {
@@ -48,4 +73,17 @@ public class Vetores {
 		}
 	}
 
+	static void listarArray(int[] dados) {
+		for(int n : dados) {
+			System.out.println(n);
+		}
+	}
+	
+	static int somar(int n1, int n2) {
+		int x = n1 + n2;
+		return x;
+	}
+	
+	
+	
 }
